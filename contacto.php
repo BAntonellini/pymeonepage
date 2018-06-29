fb367a022f3427c6aa80a49e008ef11fa5cc1fbd
BAntonellini <antonellinibruno@gmail.com>
2018-06-29 17:46:47 -0300

    1.0.0 (Launch)

--- contacto.php
+++ contacto.php
@@ -8,15 +8,14 @@ if(isset($_POST['submit'])){
     $tel = $_POST['telefono'];
     $asunto = "Contacto Megatech PyMES";
     $asunto2 = "Copia de su contacto con Megatech PyMES";
-    $message = $nombre . ", numero de contacto " .$tel . ", dejo el siguiente mensaje en " .$pagContacto. "  : " . "\n\n" . $_POST['comentario'] . "\n\n Correo de contacto: " . $email;
+    $message = $nombre . ", numero de contacto " .$tel . ", dejo el siguiente mensaje en " .$pagContacto. ": " . "\n\n" . $_POST['comentario'] . "\n\n Correo de contacto: " . $email;
     $message2 = "Le dejamos una copia del mensaje de contacto enviado a Megatech PyMES " . $first_name . "\n\n" . $_POST['comentario'];
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
 
     mail($destinatario,$subject,$message,$headers);
     mail($remitente,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-
-    header('Location: gracias.html');
+ 
+    header('Location: http://plan-pymes.com');
 }
 ?>
-
