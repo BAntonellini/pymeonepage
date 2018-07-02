20235c1e740e1f34fb35afd8f5c59be51780e5b5
BAntonellini <antonellinibruno@gmail.com>
2018-07-02 15:35:13 -0300

    PyME Onepage 1.0.0

--- mobile/mobile.js
+++ mobile/mobile.js
@@ -13,12 +13,15 @@ $('button').click(function (e) {
     switch (idBoton) {
         case 'boton-sp':
             $('#banner').attr('src', '../img/banners/mobile/Banners Cel Pymes.jpg');       
+            $('#link-banner').attr('href', '../servicios/soporte-pymes.html')
             break;
         case 'boton-cpc':
             $('#banner').attr('src', '../img/banners/mobile/Banners Cel CPC.JPG');
+            $('#link-banner').attr('href', '../servicios/costo-por-copia.html')
             break;
         case 'boton-cps':
             $('#banner').attr('src', '../img/banners/mobile/Banners Cel CPS.JPG');
+            $('#link-banner').attr('href', '../servicios/costo-por-servicio.html')
             break;
         default:
             break;
